5de4e0e714889a6d411fea5f44ac7dfc50a048a2
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2022-03-03 15:36:08 +0200

    after TheRoom POJO

--- server.js
+++ server.js
@@ -2,90 +2,125 @@ const express = require("express");
 const socket = require("socket.io");
 const cors = require("cors");
 const app = express();
+const TheRoom = require('./TheRoom.js');//ref myGame from its class file
+
 app.use(cors());
 
-var roomss = [];
-var roomCnt = new Map(); //keeps rooms count of players
-var myCounter = 0; //keeps incrementing a room
-
-var toBsent = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]; //0-11 need 12 elements
+var myCounter = 0; 
+var roomArr=[];
 
 app.use(express.static("public"));
 
 const server = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + server.address().port);
+  console.log("Your app is listening on port " + server.address().port);  
 });
 
 var io = socket(server);
 io.on("connection", function (socket) {
   console.log("Player Connected...." + socket.id); //Not sending anything back cz by default a connected event is raised at the client... so i'll know
-
-  var myOwnRoom; //a seperate copy of this variable will belong to individual sockets
+  var myOwnRoom;  
+  
 
   socket.on("RoomJoining", function (data) {
     console.log("JoiningRequest from " + data);
     socket.join(data);
-    console.log("Server rooms variable:" + socket.rooms);
-    //for (const room of socket.rooms) { console.log("===> " + room);} //room only contains its own socketId and the name of the room that this socket connected to
     socket.emit("RoomJoining", "Joined room #" + data);
     myOwnRoom = data; //used incase of sudden disconnection
-
-    //for 2x2's all players to start when room is full xxxx-- needs game2x2 to be sent from client too 
-    //OR... may be 1x1 creator is always ply1... so the moment pl2 gets this reply... it asks him to start
-    //BUT in 2x2 the clientside wont do that.. it only continues to add players. Except when the server sends back GOGOGO to everyone
-    if (roomCnt.get(myOwnRoom) == 4) {
-      console.log("GO GO GO");
-      io.in(myOwnRoom).emit("RoomJoining", "Go");
+    console.log("postROOMJoined: " + myOwnRoom);
+    
+    if(roomArr[data].count==4){
+      roomArr[data].toBsent=[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1];
+      io.in(data).emit("RoomJoining", "Go");   
+      console.log("GO GO GO");      
     }
   });
+  
 
   //Int Array data 1x1
   socket.on("myMoves", function (data) {
     console.log(data);
     socket.to(data[3]).emit("myMoves", data); //data[3]will always be roomNo
-    //socket.to(data[3]).emit('myMoves', data);//send to all clients in ROOM except the sender
-    //socket.broadcast.emit('myMoves', data);//this is for everyone BUT the sender
-    //socket.emit('myMoves', data); // back only to sender
-    //io.in("gameRoomId").emit("big-announcement", "the game will start soon");//everyone in room including sender
-    if (data[0] === 999) {
-      if (roomss[data[3]] != undefined) { //that means it should not have been already deleted
-        delete roomss[data[3]];
-        roomCnt.delete(data[3]);
+    
+    if (data[0] === 999) {//only for regular victory reset will happen from here. BUT not for sudden disconnection... cz in that case 999 wont be received here
+      if(roomArr[myOwnRoom]!=undefined){  
+        delete roomArr[data[3]];         
         console.log("reset from 999");
       }
     } //delete stuff after last move
   });
+  
 
   //Int Array data 2x2 server receives arr[0-4] and sendsback[1,2,3]cz plyNum & RoomNum doesnt need to be sentback
   socket.on("myMoves2x2", function (data) {
-    console.log(data); //[plyNum,what,move,taken,room]
+    console.log("Received: " + data); //[plyNum,what,move,taken,room]... data[4] will always be the room#
 
     var start = data[0] * 3;
-    toBsent[start] = data[1];
-    toBsent[start + 1] = data[2];
-    toBsent[start + 2] = data[3];
+    roomArr[data[4]].toBsent[start] = data[1];
+    roomArr[data[4]].toBsent[start + 1] = data[2];
+    roomArr[data[4]].toBsent[start + 2] = data[3];
+    var toBsentTemp = roomArr[data[4]].toBsent;
+    
+    console.log("SettingUp >>  " + toBsentTemp);   
+    
 
-    if (toBsent.includes(-1) == false) {
-      //only if all the -1 dont exist that means all players moves are filled in
-      io.in(data[4]).emit("myMoves2x2", toBsent); //everyone in room including sender
-      console.log("sent to ALL: " + toBsent);
-      toBsent = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]; //after it is filled by all you can reset it
-    }
-    //if(data[0]===999){roomss[data[3]] = 0;}//reset it to 0 after last move
+    if (!toBsentTemp.includes(-1)) { //only if all the -1 dont exist ... that means all players moves are filled in
+      io.in(data[4]).emit("myMoves2x2", toBsentTemp); //everyone in room including sender
+      console.log("---------sent to ALL: " + toBsentTemp);   
+      
+      //How to reset the my2dArr[room#] thats keeping track of the moves for the next set of moves to be filled in
+      //1.if game is going on normally with no deaths OR the game is fully ended... for both cases reinitialise the my2dArr 
+      //2.if there may be a few finishes but the game still hasnt ended, then we cant make those deaths into -1 cz you will lose their information
+      let finishes = toBsentTemp.filter(x => x === 999).length;          
+      if(!toBsentTemp.includes(999)){ roomArr[data[4]].toBsent = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1];}//regular reset
+      else if( finishes===3 ||  (toBsentTemp[0]===999 && toBsentTemp[3]===999) || (toBsentTemp[6]===999 && toBsentTemp[9]===999) ){
+       //if not a single 999(finish)is there(means game is on) ...OR... (3 finishes... OR ...2 finishes of the same team)... means game is over  
+        delete roomArr[data[4]];//resetted room variable
+      }                
+      else{
+          //but if 999 is there & the Game hasnt ended.. dont disturb those 3vals.. and put -1 for the others
+          for(var i =0; i<12; i++ ){
+            if(toBsentTemp[i]===999){i+=2;}//dont touch (this value & next 2 vals) of this finished player
+            else{toBsentTemp[i] = -1;}   //any other that isnt 999(& its 2 other vals) must be reset to -1
+          }
+        roomArr[data[4]].toBsent = toBsentTemp;//special reset taken place
+      }
+      
+    console.log("resetted toBsent: " + toBsentTemp);      
+    }      
   });
 
+  
+  
+  //For Cheater who shuts browser... server will receive normal 1x1 moves with 999 in data[0]... & send this to other player, so YOUWON can be displayed
+  socket.on("Cheater1x1", function (data) {//here you can do a reset cz... the other player will never be needed to reply
+    console.log("From Cheater1x1... " + data); 
+    socket.to(data[3]).emit("myMoves", data);
+    delete roomArr[data[3]];
+    console.log("reset from Cheater1x1");
+  });
+
+
+  //For Cheater who shuts browser... server will receive [positionInArray,present,taken].  toBsent will set [999, present, taken]  
+  socket.on("Cheater2x2", function (data) {
+    console.log("From Cheater... " + data); 
+
+    var start = data[0];//this is already the plyNum*3.. so server doesnt need to do calculations to find the position in toBeSent
+    roomArr[myOwnRoom].toBsent[start] = 999;//bcz he cheated
+    roomArr[myOwnRoom].toBsent[start + 1] = data[1];
+    roomArr[myOwnRoom].toBsent[start + 2] = data[2];
+    
+    //Now toBsent will never have -1,-1,-1 at the position of the player who discnnted, hence it will never wait
+    console.log("toBsent reset from Cheater: " + roomArr[myOwnRoom].toBsent);//for that players moves..& send the moves to everyone
+  });
+  
+  
   socket.on("disconnect", function () {
-    console.log("Player Disconnected x-x-x-x-x-x from Room#" + myOwnRoom);
-    if (roomss[myOwnRoom] != undefined) {
-      //that means it should not have been already deleted
-      delete roomss[myOwnRoom];
-      roomCnt.delete(myOwnRoom);
-      console.log("reset from DISCONNECT");
-    } //delete after last move in case it dint make it to move 999
-    toBsent = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1];//this could give an error if a ply discnnts during the game as toBsent is still required by the others
+    console.log("Player Disconnected x-x-x-x-x-x from Room#" + myOwnRoom);    
     console.log(socket.rooms);
   });
-});
+
+});//socket ends herexxxxxxxxxxxxx
+
 
 //https://excellent-heartbreaking-soup.glitch.me/auth?room=1&pk=35217&gm=2
 app.get("/auth", (request, response) => {
@@ -96,13 +131,10 @@ app.get("/auth", (request, response) => {
 
   response.setHeader("Content-Type", "application/json");
 
-  if (roomss[para1] === para2) {
-    let cnt = roomCnt.get(para1);
-    cnt++;
-    roomCnt.set(para1, cnt);
-    console.log("Room count..." + roomCnt.get(para1));
-
-    //response.send(JSON.stringify({message: 'Access Granted',ply: cnt}));
+  if (roomArr[para1].pk === para2) {//authPassed!
+    roomArr[para1].count++;
+    let cnt = roomArr[para1].count;
+    console.log("Room count..." + roomArr[para1].count);
 
     if (para3 === 2) {
       if (cnt <= 4) {
@@ -117,10 +149,11 @@ app.get("/auth", (request, response) => {
         response.send(JSON.stringify({ message: "Access Denied", ply: -1 }));
       } //-1 means nothing
     }
-  } else {
-    //authFAILED
+    
+  } else { //authFAILED
     response.send(JSON.stringify({ message: "Access Denied" }));
   }
+    
 });
 
 //https://excellent-heartbreaking-soup.glitch.me/create
@@ -130,14 +163,13 @@ app.get("/create", (request, response) => {
   response.setHeader("Content-Type", "application/json");
 
   myCounter++;
-  roomCnt.set(myCounter, 1); //1st guy who created the room
   let k = Math.floor(Math.random() * 10000 + 1);
-  roomss[myCounter] = k;
+  var obj = new TheRoom(myCounter,k);
+  obj.count = 1; //first guy
+  roomArr[myCounter] = obj;
 
-  console.log(
-    "Room#..." + myCounter + "|key " + roomss[myCounter] + "|ply#  " + roomCnt.get(myCounter));
+  console.log("Room#..." + roomArr[myCounter].id  + "|key " + roomArr[myCounter].pk  + "|ply#  " + roomArr[myCounter].count );
 
   response.send(JSON.stringify({ message: "Created", room: myCounter, pk: k }));
 });
-
 
